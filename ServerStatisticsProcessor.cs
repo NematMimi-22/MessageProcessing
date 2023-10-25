d5c5ccc5dffdb68e0c95e7a676d183210d4affc3
Nemat Mimi <nematmimi01@gmail.com>
2023-10-25 15:50:21 +0300

    Remove Comments Code

--- ServerStatisticsProcessor.cs
+++ ServerStatisticsProcessor.cs
@@ -1,37 +1,6 @@
 ﻿using Weather_Monitoring.ReadConfig;
 public class ServerStatisticsProcessor
 {
-    //private readonly IMongoDBService _mongoDBService;
-    //   private readonly IMessageQueueService _messageQueueService;
-    //  private readonly IHubContext<AlertHub> _hubContext;
-    //private readonly AnomalyDetectionConfig _anomalyDetectionConfig;
-
-    /*  public ServerStatisticsProcessor( IOptions<AnomalyDetectionConfig> anomalyDetectionConfig)
-      {
-
-      }
-
-      public async Task StartProcessingAsync()
-      {
-          await _messageQueueService.StartReceivingMessagesAsync();
-      }
-
-      public async Task ProcessMessageAsync(ServerStatistics data)
-      {
-          // Persist data to MongoDB
-          await _mongoDBService.InsertServerStatisticsAsync(data);
-
-          // Detect anomalies and send alerts
-          if (IsMemoryUsageAnomaly(data) || IsCpuUsageAnomaly(data))
-          {
-              await _hubContext.Clients.All.SendAsync("ReceiveAnomalyAlert", data);
-          }
-          else if (IsMemoryHighUsage(data) || IsCpuHighUsage(data))
-          {
-              await _hubContext.Clients.All.SendAsync("ReceiveHighUsageAlert", data);
-          }
-      }*/
-
     private bool IsMemoryAnomaly(ServerStatistics current, ServerStatistics previous)
     {
         return current.MemoryUsage > (previous.MemoryUsage * (1 + ConfigReader.ReadAnomalyDetectionConfig().MemoryUsageAnomalyThresholdPercentage));

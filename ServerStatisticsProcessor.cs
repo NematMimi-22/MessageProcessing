fdca3b0e58b39c3257f204ca86c82a419f8288fe
Nemat Mimi <nematmimi01@gmail.com>
2023-10-25 14:57:41 +0300

    Equations For Alert Calculation

---
+++ ServerStatisticsProcessor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Options;
+
+public class ServerStatisticsProcessor
+{/*
+    //private readonly IMongoDBService _mongoDBService;
+ //   private readonly IMessageQueueService _messageQueueService;
+  //  private readonly IHubContext<AlertHub> _hubContext;
+    //private readonly AnomalyDetectionConfig _anomalyDetectionConfig;
+
+    public ServerStatisticsProcessor( IOptions<AnomalyDetectionConfig> anomalyDetectionConfig)
+    {
+
+    }
+
+    public async Task StartProcessingAsync()
+    {
+        await _messageQueueService.StartReceivingMessagesAsync();
+    }
+
+    public async Task ProcessMessageAsync(ServerStatistics data)
+    {
+        // Persist data to MongoDB
+        await _mongoDBService.InsertServerStatisticsAsync(data);
+
+        // Detect anomalies and send alerts
+        if (IsMemoryUsageAnomaly(data) || IsCpuUsageAnomaly(data))
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveAnomalyAlert", data);
+        }
+        else if (IsMemoryHighUsage(data) || IsCpuHighUsage(data))
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveHighUsageAlert", data);
+        }
+    }
+
+    private bool IsMemoryUsageAnomaly(ServerStatistics data)
+    {
+        // Implement memory usage anomaly logic
+        // Return true if anomaly is detected, false otherwise
+    }
+
+
+    private bool IsCpuUsageAnomaly(ServerStatistics data)
+    {
+        // Implement CPU usage anomaly logic
+        // Return true if anomaly is detected, false otherwise
+    }
+
+    private bool IsMemoryHighUsage(ServerStatistics data)
+    {
+        // Implement memory high usage logic
+        // Return true if high usage is detected, false otherwise
+    }
+
+    private bool IsCpuHighUsage(ServerStatistics data)
+    {
+        // Implement CPU high usage logic
+        // Return true if high usage is detected, false otherwise
+    }*/
+}

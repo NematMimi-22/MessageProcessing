094a2443370385640644694e3f910f11b3321f6f
Nemat Mimi <nematmimi01@gmail.com>
2023-10-25 11:08:25 +0300

    Create Json File And ServerStatistics Class

---
+++ ServerStatistics.cs
@@ -0,0 +1,8 @@
+﻿public class ServerStatistics
+{
+    public String ServerIdentifier { get; set; }
+    public double MemoryUsage { get; set; } // in MB
+    public double AvailableMemory { get; set; } // in MB
+    public double CpuUsage { get; set; }
+    public DateTime Timestamp { get; set; }
+}
